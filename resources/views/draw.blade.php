6b8116915f349ca9b9a89841a2694566e27cb5e5
MJTRABAHO <mjdurandarjr@gmail.com>
2023-03-15 16:17:48 +0800

    FEAT: create p tag

--- resources/views/draw.blade.php
+++ resources/views/draw.blade.php
@@ -3,6 +3,8 @@
 
 
 @section('content')
+<!-- add paragraph by mj  -->
+<p style="font-size: 50px;">mj josh pat alex sean lesther renzon</p>
     <div id="app">
         <draw-component
             :members = "{{$getMembers}}"
