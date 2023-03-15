a2752aee570c1cc66617824dc36862084b3499d1
greenappletech-dev <arnold.estores@greenappletech.ph>
2023-03-15 16:10:42 +0800

    Feat: Add H3 in Draw

--- resources/views/draw.blade.php
+++ resources/views/draw.blade.php
@@ -3,6 +3,10 @@
 
 
 @section('content')
+
+    {{--  lesther create h3 in draw blade  --}}
+    <h3>Hello World!</h3>
+
     <div id="app">
         <draw-component
             :members = "{{$getMembers}}"
